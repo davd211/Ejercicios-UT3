535e62c28732dc25e84d52e62fa9273968d8ad15
davd211 <149159001+davd211@users.noreply.github.com>
2023-11-13 19:14:21 +0100

    Ejercicio3AMPLI2.java

---
+++ Ejercicio3AMPLI2.java
@@ -0,0 +1,23 @@
+public class Ejercicio3AMPLI2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        int numero1 = 6;
+        int numero2 = 2;
+        int resultado=1;
+        
+        for(int i=0;i<numero2;i++){
+            
+            resultado *= numero1;
+
+        }                System.out.println("3 elevado a 2 es igual a "+resultado);
+
+            
+            
+
+    }
+
+}
