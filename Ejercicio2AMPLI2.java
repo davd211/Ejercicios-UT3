f92e69166e2ce33874e5f46dbb97529e40ba699b
davd211 <149159001+davd211@users.noreply.github.com>
2023-11-13 18:46:24 +0100

    Ejercicio2AMPLI2.java

---
+++ Ejercicio2AMPLI2.java
@@ -0,0 +1,40 @@
+public class Ejercicio2AMPLI2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        int pares = 0, impares = 0;
+        int suma = 0;
+
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 == 0) {
+                pares=i+pares;
+            } else {
+                impares=i+impares;
+            }
+
+        }
+        System.out.printf("Pares suman "+pares+" e impares suman "+impares);
+    }
+
+} 
+//    public static void main(String[] args) {
+//        int sumaPares = 0;
+//        int sumaImpares = 0;
+//
+//        for (int i = 1; i <= 100; i++) {
+//            if (i % 2 == 0) {
+//                // Si es par
+//                sumaPares += i;
+//            } else {
+//                // Si es impar
+//                sumaImpares += i;
+//            }
+//        }
+//
+//        System.out.println("Suma de números pares: " + sumaPares);
+//        System.out.println("Suma de números impares: " + sumaImpares);
+//    }
+//}
