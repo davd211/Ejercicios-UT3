df6ef4729aef464f860699a4a917a50e2c1eecb1
davd211 <149159001+davd211@users.noreply.github.com>
2023-11-13 19:20:22 +0100

    ejercicio4AMPLI2.java

---
+++ ejercicio4AMPLI2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author David.P
+ */
+public class ejercicio4AMPLI2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        int numero1 = teclado.nextInt();
+        int numero2 = teclado.nextInt();
+        int numero3 = teclado.nextInt();
+        int numero4 = teclado.nextInt();
+        
+        System.out.printf("%d%d%d%d",numero1,numero2,numero3,numero4);
+
+    }
+
+}
